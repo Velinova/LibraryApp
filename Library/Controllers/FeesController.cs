7382ad031c40c3e65b29b565b2068ee8899cd444
Viktorija Velinova <viktorijavelinovaa@gmail.com>
2020-03-23 22:35:40 +0100

    Added fees view and controller

---
+++ Library/Controllers/FeesController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Library;
+using Microsoft.AspNetCore.Http;
+
+namespace Library.Controllers
+{
+    public class FeesController : Controller
+    {
+        private readonly db_201920z_va_prj_my_libContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public FeesController(db_201920z_va_prj_my_libContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+            ViewBag.UserId = _httpContextAccessor.HttpContext.Request.Cookies["id"];
+            ViewBag.UserName = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            ViewBag.UserRole = _httpContextAccessor.HttpContext.Request.Cookies["userrole"];
+        }
+
+        // GET: Fees
+        public async Task<IActionResult> Index()
+        {
+            var db_201920z_va_prj_my_libContext = _context.Fees.Include(f => f.Borrow);
+            ViewBag.UserId = _httpContextAccessor.HttpContext.Request.Cookies["id"];
+            ViewBag.UserName = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            ViewBag.UserRole = _httpContextAccessor.HttpContext.Request.Cookies["userrole"];
+            return View(await db_201920z_va_prj_my_libContext.ToListAsync());
+        }
+
+        // GET: Fees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fees = await _context.Fees
+                .Include(f => f.Borrow)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fees == null)
+            {
+                return NotFound();
+            }
+            ViewBag.UserId = _httpContextAccessor.HttpContext.Request.Cookies["id"];
+            ViewBag.UserName = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            ViewBag.UserRole = _httpContextAccessor.HttpContext.Request.Cookies["userrole"];
+            return View(fees);
+        }
+
+        // GET: Fees/Create
+        public IActionResult Create()
+        {
+            ViewData["Borrowid"] = new SelectList(_context.Borrowings, "Id", "Id");
+            ViewBag.UserId = _httpContextAccessor.HttpContext.Request.Cookies["id"];
+            ViewBag.UserName = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            ViewBag.UserRole = _httpContextAccessor.HttpContext.Request.Cookies["userrole"];
+            return View();
+        }
+
+        // POST: Fees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Feetotal,Borrowid")] Fees fees)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(fees);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Borrowid"] = new SelectList(_context.Borrowings, "Id", "Id", fees.Borrowid);
+            ViewBag.UserId = _httpContextAccessor.HttpContext.Request.Cookies["id"];
+            ViewBag.UserName = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            ViewBag.UserRole = _httpContextAccessor.HttpContext.Request.Cookies["userrole"];
+            return View(fees);
+        }
+
+        // GET: Fees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fees = await _context.Fees.FindAsync(id);
+            if (fees == null)
+            {
+                return NotFound();
+            }
+            ViewData["Borrowid"] = new SelectList(_context.Borrowings, "Id", "Id", fees.Borrowid);
+            ViewBag.UserId = _httpContextAccessor.HttpContext.Request.Cookies["id"];
+            ViewBag.UserName = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            ViewBag.UserRole = _httpContextAccessor.HttpContext.Request.Cookies["userrole"];
+            return View(fees);
+        }
+
+        // POST: Fees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Feetotal,Borrowid")] Fees fees)
+        {
+            if (id != fees.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fees);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FeesExists(fees.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Borrowid"] = new SelectList(_context.Borrowings, "Id", "Id", fees.Borrowid);
+            ViewBag.UserId = _httpContextAccessor.HttpContext.Request.Cookies["id"];
+            ViewBag.UserName = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            ViewBag.UserRole = _httpContextAccessor.HttpContext.Request.Cookies["userrole"];
+            return View(fees);
+        }
+
+        // GET: Fees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fees = await _context.Fees
+                .Include(f => f.Borrow)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fees == null)
+            {
+                return NotFound();
+            }
+            ViewBag.UserId = _httpContextAccessor.HttpContext.Request.Cookies["id"];
+            ViewBag.UserName = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            ViewBag.UserRole = _httpContextAccessor.HttpContext.Request.Cookies["userrole"];
+            return View(fees);
+        }
+
+        // POST: Fees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var fees = await _context.Fees.FindAsync(id);
+            _context.Fees.Remove(fees);
+            await _context.SaveChangesAsync();
+            ViewBag.UserId = _httpContextAccessor.HttpContext.Request.Cookies["id"];
+            ViewBag.UserName = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            ViewBag.UserRole = _httpContextAccessor.HttpContext.Request.Cookies["userrole"];
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FeesExists(int id)
+        {
+            ViewBag.UserId = _httpContextAccessor.HttpContext.Request.Cookies["id"];
+            ViewBag.UserName = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            ViewBag.UserRole = _httpContextAccessor.HttpContext.Request.Cookies["userrole"];
+            return _context.Fees.Any(e => e.Id == id);
+        }
+    }
+}
